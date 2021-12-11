fc8447886eadbe067125b155cf827e0512c9aa88
PankajSingh1010 <pankajsingh132000@gmail.com>
2021-12-10 23:24:22 -0500

    symbl library has been added and succesfull create the conversation object

--- subtitleGenerator.py
+++ subtitleGenerator.py
@@ -1,19 +1,68 @@
 #!/bin/python
 
-import argparse
+try:	
+	import argparse
+	import symbl
+	parser = argparse.ArgumentParser(description="Subtite Generator Using Symbl.ai")
 
-def argument_function():
-	try:	
-		parser = argparse.ArgumentParser(description="Subtite Generator Using Symbl.ai")
+	parser.add_argument('--filetype', type=str, required=True, help="Give file type audio/video")
+	parser.add_argument('--locate', type=str, required=True, help="Locate file")
+	parser.add_argument('--output', type=str, default='outputfile.srt', help="Name of Output file")
+	args = parser.parse_args()
+		
+except ModuleNotFoundError as err:
+	print(err)
+	exit()
+except Exception as e:
+	print(e)
+	print("An Error has occur...Please try again")
+	exit()
 
-		parser.add_argument('--filetype', type=str, required=True, help="Give Either audio or video")
-		parser.add_argument('--location', type=str, required=True, help="Location of file")
-		parser.add_argument('--output', type=str, default='outputfile.srt', help="Name of Output file")
-		args = parser.parse_args()
-	except:
-		print("An Error has occur...Please restart")
 
+def generate_conversation_object(file_type, location):
+	
+	if(file_type=="Audio"):
+		try:
+			print("Your Subtitle File Is Generating")
+			print("Please Wait This Might Take A While...")
+			conv_object = symbl.Audio.process_file(file_path=location)
+			return conv_object
+		except Exception as e:
+			print(e)
+			print("Something went wrong please check the file and try again")
+			exit()
+	else:
+		try:
+			print("Your Subtitle File Is Generating")
+			print("Please Wait This Might Take A While.....")
+			conv_object = symbl.Video.process_file(file_path=location)
+			return conv_object
+		except Exception as e:
+			print(e)
+			print("Something went wrong please check the file and try again")
+			exit()
+
+def main():	
+	file_type = args.filetype
+	location = args.locate
+	output_filename = args.output
+	print(file_type,location,output_filename)
+	
+	filetype_list = ['audio','video','Audio','Video']
+	if(file_type in filetype_list):
+		conversation_object = generate_conversation_object(file_type.capitalize(), location)
+	else:
+		print("filetype is not audio/video")
+		exit()
+		
+	print(conversation_object.get_messages().messages[0].text)
+	print(conversation_object.get_messages().messages[0].start_time.time())
+	print(conversation_object.get_messages().messages[0].end_time.time())
+			 
+	
+	
+		
 
 
 if __name__=="__main__":
-	augument_function()
+	main()
