71179609ad31db4506a9f86ca83c581e2e090cf3
PankajSingh1010 <pankajsingh132000@gmail.com>
2021-12-11 14:18:54 -0500

    Some files have been modified and Project is complete now

--- subtitleGenerator.py
+++ subtitleGenerator.py
@@ -45,12 +45,36 @@ def generate_conversation_object(file_type, location):
 
 def generate_output_file(conv_obj, output_filename):
 	
-	err_hour = int(conv_obj.get_messages().messages[0].start_time.hour) #these values are not part of file
+	#these value are errors ... or not part of timestamps
+	err_hour = int(conv_obj.get_messages().messages[0].start_time.hour) #these values are not part of time stamps
 	err_minute = int(conv_obj.get_messages().messages[0].start_time.minute) 
 	err_second = int(conv_obj.get_messages().messages[0].start_time.second) 
 	
-	print(err_hour,err_minute,err_second)
 	
+	#This will generate simple .srt file with default timestamps
+	# with open(output_filename, 'w') as f:
+	# 	sequence_number = 1
+	# 	for data in conv_obj.get_messages().messages:
+	# 		f.write(f'{str(sequence_number)}\n')
+	# 		s_hour = int(data.start_time.hour)
+	# 		e_hour = int(data.end_time.hour)
+			
+	# 		s_minute = int(data.start_time.minute)
+	# 		e_minute = int(data.end_time.minute)
+			
+	# 		s_second = int(data.start_time.second)
+	# 		e_second = int(data.end_time.second)
+			
+	# 		f.write(f'{s_hour}:{s_minute}:{s_second} --> {e_hour}:{e_minute}:{e_second}\n')
+
+	# 		f.write(f'{data.text}\n\n')
+	# 		sequence_number += 1
+			
+	# 	print("Task Complete..Subtitle File Has Been Created")
+	
+	
+	#To make make our output more accurate we have subtract error timestamps from the default timestamps
+	#this will generate the more accurate subtitle file
 	with open(output_filename, 'w') as f:
 		sequence_number = 1
 		for data in conv_obj.get_messages().messages:
@@ -65,27 +89,28 @@ def generate_output_file(conv_obj, output_filename):
 			e_second = int(data.end_time.second)
 			
 			f.write(f'{s_hour-err_hour}:{s_minute-err_minute}:{s_second-err_second} --> {e_hour-err_hour}:{e_minute-err_minute}:{e_second-err_second}\n')
-			print(data.text)
 			f.write(f'{data.text}\n\n')
 			sequence_number += 1
-			
-		print("Take Complete..Subtitle File Is Created")
+		print("Task Complete..Subtitle File Has Been Created")
+
 
 def main():	
 	file_type = args.filetype
 	location = args.locate
 	output_filename = args.output
-	print(file_type,location,output_filename)
+	print("Filetype -->",file_type)
+	print("File location -->",location)
+	print("Outputfile name -->",output_filename)
 	
 	filetype_list = ['audio','video','Audio','Video']
 
 	if(file_type in filetype_list):
-		conversation_object = generate_conversation_object(file_type.capitalize(), location) #function call to create a conversation object
+		conversation_object = generate_conversation_object(file_type.capitalize(), location) #method call to create a conversation object
 	else:
 		print("filetype is not audio/video")
 		exit()
 		
-	generate_output_file(conversation_object, output_filename) #function call to generate subtitle file	
+	generate_output_file(conversation_object, output_filename) #method call to generate subtitle file	
 
 
 if __name__=="__main__":
